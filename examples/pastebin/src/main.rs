65cecca87e6bdac8d57f9f349e2d4eef918ace89
bill zxy <bill.zxy@qq.com>
2020-12-10 17:25:34 +0800

    Draft release of a static web serer based on Rust

---
+++ examples/pastebin/src/main.rs
@@ -0,0 +1,56 @@
+#[macro_use] extern crate rocket;
+
+mod paste_id;
+#[cfg(test)] mod tests;
+
+use std::io;
+use std::path::Path;
+use std::path::PathBuf;
+
+
+use rocket::data::{Data, ToByteUnit};
+use rocket::response::{content::Plain, Debug};
+use rocket::tokio::fs::File;
+use rocket::response::NamedFile;
+use rocket::response::status::NotFound;
+// use rocket_contrib::serve::StaticFiles;
+
+
+use crate::paste_id::PasteID;
+
+const HOST: &str = "http://localhost:8000";
+const ID_LENGTH: usize = 3;
+
+#[post("/", data = "<paste>")]
+async fn upload(paste: Data) -> Result<String, Debug<io::Error>> {
+    let id = PasteID::new(ID_LENGTH);
+    let filename = format!("upload/{id}", id = id);
+    let url = format!("{host}/{id}\n", host = HOST, id = id);
+
+    paste.open(128.kibibytes()).stream_to_file(filename).await?;
+    Ok(url)
+}
+
+#[get("/<id>",rank = 2)]
+async fn retrieve(id: PasteID<'_>) -> Option<Plain<File>> {
+    let filename = format!("upload/{id}", id = id);
+    File::open(&filename).await.map(Plain).ok()
+}
+
+#[get("/<file..>", rank =3)]
+async fn files(file: PathBuf) -> Result<NamedFile, NotFound<String>> {
+    let path = Path::new("static/").join(file);
+    NamedFile::open(&path).await.map_err(|e| NotFound(e.to_string()))
+}
+
+#[get("/")]
+async fn index() -> Result<NamedFile, NotFound<String>> {
+    let path = Path::new("static/").join("index.html");
+    NamedFile::open(&path).await.map_err(|e| NotFound(e.to_string()))
+}
+
+#[launch]
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index, upload, retrieve,files])
+   
+}
