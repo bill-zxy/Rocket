e941b0de3ae2636ef101bde4c57dce1a3b932edc
bill zxy <bill.zxy@qq.com>
2020-12-12 23:01:25 +0800

    The first release of static webserver with rust

--- examples/pastebin/src/main.rs
+++ examples/pastebin/src/main.rs
@@ -2,6 +2,7 @@
 #[macro_use] extern crate rocket;
 extern crate rand;
 
+
 mod paste_id;
 #[cfg(test)] mod tests;
 
@@ -10,6 +11,7 @@ use std::fs::File;
 use std::path::Path;
 use std::path::PathBuf;
 use std::result::Result;
+use std::net::UdpSocket;
 
 use rocket::Data;
 use rocket::response::{content, Debug};
@@ -18,10 +20,10 @@ use rocket::response::NamedFile;
 use paste_id::PasteID;
 use rocket::config::{Config,Environment};
 
-
 const HOST: &str = "http://localhost:8080";
 const ID_LENGTH: usize = 3;
 
+/*
 #[post("/", data = "<paste>")]
 fn upload(paste: Data) -> Result<String, Debug<io::Error>> {
     let id = PasteID::new(ID_LENGTH);
@@ -37,29 +39,52 @@ fn retrieve(id: PasteID<'_>) -> Option<content::Plain<File>> {
     let filename = format!("upload/{id}", id = id);
     File::open(&filename).map(|f|Plain(f)).ok()
 }
-
-#[get("/<file..>", rank =3)]
+*/
+#[get("/<file..>")]
 
 fn files(file: PathBuf) -> Option<NamedFile>  {
-   NamedFile::open(Path::new("/service/webserver/static/").join(file)).ok()
+   NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
 
 #[get("/")]
 fn index() -> Option<NamedFile>  {
-   NamedFile::open(Path::new("/service/webserver/static/index.html")).ok()
+   NamedFile::open(Path::new("static/index.html")).ok()
 }
 
  
 fn rocket() -> rocket::Rocket {
     
+    let ip = get_ipaddress().unwrap();
     let config = Config::build(Environment::Production)
-     .unwrap();
+    .address(ip)
+    .port(8080) 
+    .unwrap();
         
-    rocket::custom(config).mount("/",routes![index, upload, retrieve,files])
+    rocket::custom(config).mount("/",routes![index, files])
 }
 
 fn main() {
     
     rocket().launch();
+}
+
+
+
+/// get the local ip address, return an `Option<String>`. when it fail, return `None`.
+fn get_ipaddress() -> Option<String> {
+    let socket = match UdpSocket::bind("0.0.0.0:0") {
+        Ok(s) => s,
+        Err(_) => return None,
+    };
+
+    match socket.connect("8.8.8.8:80") {
+        Ok(()) => (),
+        Err(_) => return None,
+    };
+
+    match socket.local_addr() {
+        Ok(addr) => return Some(addr.ip().to_string()),
+        Err(_) => return None,
+    };
 }
